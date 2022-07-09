459e529126a298098356a92155141de25d92c52d
Joao <jp.alvesrodrigues02@gmail.com>
2022-07-09 00:55:19 -0300

    tempo max min e reset

--- js/pomodoro-timer.js
+++ js/pomodoro-timer.js
@@ -1,6 +1,7 @@
 
 var start = document.getElementById('start') // botão start timer
 var pause = document.getElementById('pause') // botão pause timer
+var reset = document.getElementById('reset')
 
 var wm = document.getElementById('w_minutes');
 var ws = document.getElementById('w_seconds');
@@ -36,13 +37,48 @@ function timer(){
     }
 }
 
+// Pausa o timer
 pause.addEventListener('click', function(){
+    pause.style = "display: none;"
+    reset.style = "dispay: flex;"
     pauseInterval()
     startTimer = undefined;
 })
 
 function pauseInterval(){
     clearInterval(startTimer);
+}
+
+// resetar o tempo do timer
+
+reset.addEventListener('click', function(){
+    pause.style = "display: flex;"
+    reset.style = "dispay: none;"
+    restInterval();
+    startTimer = undefined;
+})
+
+function restInterval(){
+    clearInterval(startTimer);
+    const time_minutes = document.querySelector("#time-minutes");
+    const time_seconds = document.querySelector("#time-seconds");
+    wm.textContent = time_minutes.value;
+    ws.textContent = time_seconds.value;
+    // Adiciona um 0 na frente de numeros menores que 10
+    if(ws.textContent < 10){
+        ws.textContent =  "0" + ws.textContent
+    }
+
+    if(wm.textContent < 10){
+        wm.textContent =  "0" + wm.textContent
+    }
+
+    if(ws.textContent == "" ||ws.textContent == " " || ws.textContent == "0"){
+        ws.textContent =  "00"
+    }
+    if(wm.textContent == "" ||wm.textContent == " " || wm.textContent == "0"){
+        wm.textContent =  "00"
+    }
 }
 const save = document.querySelector("#send") // botão de enviar o tempo 
 
@@ -51,9 +87,25 @@ save.addEventListener("click",function(e){
 
     const time_minutes = document.querySelector("#time-minutes");
     const time_seconds = document.querySelector("#time-seconds");
-    ws.textContent = time_seconds.value;
-    wm.textContent = time_minutes.value;
+    console.log(time_minutes)
+    if(time_minutes.value > 99){ // limite maximo de tempo em minutos = 99
+        time_minutes.value = 99;
+        window.alert("Max minutes value is 99 ");
 
+    }else{
+        wm.textContent = time_minutes.value;
+    }
+
+    if(time_seconds.value > 59){ // limite maximo de tempo em segundos = 59
+        time_seconds.value = 59;
+        window.alert("Max Secods value is 99 ");
+
+    }else{
+        ws.textContent = time_seconds.value;
+    }
+    
+
+    // Adiciona um 0 na frente de numeros menores que 10
     if(ws.textContent < 10){
         ws.textContent =  "0" + ws.textContent
     }
@@ -71,54 +123,3 @@ save.addEventListener("click",function(e){
     
 
 })
-
-
-/*
-var user_time = 0;
-var save_time = 0;
-var pomodoro;
-
-
-function timer(duration,display){
-
-    var timer = duration,minutes,seconds;
-    
-        pomodoro = setInterval(function(){
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        
-        display.textContent = minutes + ":" + seconds;
-
-        --timer;
-        
-    },1000);
-}
-
-function start(){
-    var duration = user_time;
-    var display = document.querySelector("#timer");
-    timer(duration,display)
-}
-
-const btn = document.querySelector("#send") // botão de enviar o tempo 
-
-btn.addEventListener("click",function(e){
-    e.preventDefault();
-
-    const time_minutes = document.querySelector("#time-minutes");
-    const time_seconds = document.querySelector("#time-seconds");
-
-    var minute_user = time_minutes.value * 60;
-    user_time = parseInt(minute_user) + parseInt(time_seconds.value);
-    console.log(user_time);
-
-})
-
-function pause(){
-    clearInterval(pomodoro);
-    console.log(save_time);
-
-}*/
